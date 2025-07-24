1b96e023601498e1e21d5a93882e8371fea3a239
thandosim <sim24001@byui.edu>
2025-07-24 13:47:50 +0200

    Fix login route to use passport authentication and implement logout functionality

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,16 @@ router.use('/', require('./swagger'));
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 
-router.get('/login', passport(req, res){
+router.get('/login', passport.authenticate('github'), (req, res) =>{});
+
+router.get('/logout', (req, res) => {
+    req.logOut((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 
 
 module.exports = router;
